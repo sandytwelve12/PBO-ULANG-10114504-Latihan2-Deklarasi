beb8db0a61b7c323af2f94cb53f01d878716871b
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 04:40:30 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan2-Deklarasi/src/pbo/ulang/pkg10114504/latihan2/deklarasi/PBOULANG10114504Latihan2Deklarasi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan2.deklarasi;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program Deklarasi
+ */
+public class PBOULANG10114504Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //nkonstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi Nilai ke Variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI );
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
